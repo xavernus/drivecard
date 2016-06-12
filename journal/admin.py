61968db51848224fd46fd924100b021148fc8f51
avernusx <avernusx@yandex.ru>
2016-06-12 23:15:19 +0300

    Модели зарегистрированы в админке

--- journal/admin.py
+++ journal/admin.py
@@ -1,3 +1,25 @@
+# -*- coding: utf-8 -*-
 from django.contrib import admin
 
-# Register your models here.
+from .models import OfficialDealer, Detail, CarModel, Car, CarDetail
+
+class OfficialDealerAdmin(admin.ModelAdmin):
+    list_display = ('name','name')
+
+class DetailAdmin(admin.ModelAdmin):
+    list_display = ('name','name')
+
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ('name','name')
+
+class CarAdmin(admin.ModelAdmin):
+    list_display = ('name','name')
+
+class CarDetailAdmin(admin.ModelAdmin):
+    list_display = ('name','name')
+
+admin.site.register(OfficialDealer, OfficialDealerAdmin)
+admin.site.register(Detail, DetailAdmin)
+admin.site.register(CarModel, CarModelAdmin)
+admin.site.register(Car, CarAdmin)
+admin.site.register(CarDetail, CarDetailAdmin)
